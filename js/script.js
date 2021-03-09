95f0fd0a4f1fa218df583003b73936fba607dc5c
Vildan Khissamutdinov <welldonemort@gmail.com>
2021-03-09 09:44:27 +0600

    Slider UPD1

--- js/script.js
+++ js/script.js
@@ -9,58 +9,31 @@ const sliderElOne = document.querySelector('.slider-element-1');
 const sliderElTwo = document.querySelector('.slider-element-2');
 const sliderElThree = document.querySelector('.slider-element-3');
 
-setInterval(
-function slide() {
-  if (sliderElOne.style.display != 'none') {
-  arrowRight.addEventListener('click', () => {
-    sliderElOne.style.display = 'none';
-    sliderElTwo.style.display = 'block';
-    dotOne.style.opacity = 0.3;
-    dotTwo.style.opacity = 1;
-  });
-  arrowLeft.addEventListener('click', () => {
-    sliderElOne.style.display = 'none';
-    sliderElThree.style.display = 'block';
-    dotOne.style.opacity = 0.3;
-    dotThree.style.opacity = 1;
-  });
-  dotTwo.addEventListener('click', () => {
-    sliderElOne.style.display = 'none';
-    sliderElTwo.style.display = 'block';
-    dotOne.style.opacity = 0.3;
-    dotTwo.style.opacity = 1;
-  });
-  dotThree.addEventListener('click', () => {
-    sliderElOne.style.display = 'none';
-    sliderElThree.style.display = 'block';
-    dotOne.style.opacity = 0.3;
-    dotThree.style.opacity = 1;
-  });
-} else if (sliderElTwo.style.display != "none") {
-  arrowRight.addEventListener('click', () => {
-    sliderElTwo.style.display = 'none';
-    sliderElThree.style.display = 'block';
-    dotTwo.style.opacity = 0.3;
-    dotThree.style.opacity = 1;
-  });
-  arrowLeft.addEventListener('click', () => {
-    sliderElTwo.style.display = 'none';
-    sliderElOne.style.display = 'block';
-    dotTwo.style.opacity = 0.3;
-    dotOne.style.opacity = 1;
-  });
-} else if (sliderElThree.style.display != "none") {
-  arrowRight.addEventListener('click', () => {
-    sliderElThree.style.display = 'none';
-    sliderElOne.style.display = 'block';
-    dotThree.style.opacity = 0.3;
-    dotOne.style.opacity = 1;
-  });
-  arrowLeft.addEventListener('click', () => {
-    sliderElThree.style.display = 'none';
-    sliderElTwo.style.display = 'block';
-    dotThree.style.opacity = 0.3;
-    dotTwo.style.opacity = 1;
-  });
+const entities = [
+  {
+    img: './images/projects.svg'
+  },
+  {
+    img: './images/projects-1.png'
+  },
+  {
+    img: './images/projects-2.png'
+  }
+]
+
+const slider = document.querySelector('.slider');
+
+const setEntity = (index) => {
+  slider.style.backgroundImage = `url(${entities[index].img})`;
 }
-}, 0);
+
+let currentIndex = 0
+
+arrowLeft.addEventListener('click', () => {
+  setEntity(currentIndex - 1);
+  currentIndex -= 1;
+})
+arrowRight.addEventListener('click', () => {
+  setEntity(currentIndex + 1);
+  currentIndex += 1;
+})
