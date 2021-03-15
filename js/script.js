e4836afc8134601d59616239e2f2b71787422f2d
Vildan Khissamutdinov <welldonemort@gmail.com>
2021-03-15 17:32:05 +0600

    Внёс правки

--- js/script.js
+++ js/script.js
@@ -1,97 +1,112 @@
+//НАВИГАЦИЯ
 const arrowLeft = document.querySelector('.slider-arrow-left');
 const arrowRight = document.querySelector('.slider-arrow-right');
 
-const dotOne = document.querySelector('.dot-1');
-const dotTwo = document.querySelector('.dot-2');
-const dotThree = document.querySelector('.dot-3');
-
-const sliderElOne = document.querySelector('.slider-element-1');
-const sliderElTwo = document.querySelector('.slider-element-2');
-const sliderElThree = document.querySelector('.slider-element-3');
-
-const lineOne = document.querySelector('.line-decorate-1');
-const lineTwo = document.querySelector('.line-decorate-2');
-const lineThree = document.querySelector('.line-decorate-3');
-
 const entities = [
   {
-    img: './images/projects.svg'
+    img: './images/projects.svg',
+    dot: document.querySelector('.dot-1'),
+    line: document.querySelector('.line-decorate-1'),
+    city: 'Rostov-on-Don LCD admiral',
+    area: '81 m2',
+    time: '3.5 months'
   },
   {
-    img: './images/projects-1.png'
+    img: './images/projects-1.png',
+    dot: document.querySelector('.dot-2'),
+    line: document.querySelector('.line-decorate-2'),
+    city: 'Sochi Thieves',
+    area: '105 m2',
+    time: '4 months'
   },
   {
-    img: './images/projects-2.png'
+    img: './images/projects-2.png',
+    dot: document.querySelector('.dot-3'),
+    line: document.querySelector('.line-decorate-3'),
+    city: 'Rostov-on-Don Patriotic',
+    area: '93 m2',
+    time: '3 months'
   }
 ]
-
+//Обязательный блок
 const slider = document.querySelector('.slider');
 
+
+//БЛОК С ФУНКЦИЯМИ
+//1.Поменять картинку
 const setEntity = (index) => {
   slider.style.backgroundImage = `url(${entities[index].img})`;
 }
 
+//2.Неактивные -> активные элементы
+const makeActive = (index) => {
+  entities[index].dot.style.opacity = 1;
+  entities[index].line.classList.add('brown-hypertext');
+}
+
+//3.Активные -> неактивные элементы
+const makeInactive = (index) => {
+  entities[index].dot.style.opacity = 0.3;
+  entities[index].line.classList.remove('brown-hypertext');
+}
+
+//4.Смена текстового содержимого
+const changeTextContent = (index) => {
+  document.querySelector('.city-text').textContent = entities[index].city;
+  document.querySelector('.area').textContent = entities[index].area;
+  document.querySelector('.time').textContent = entities[index].time;
+}
+
+//5.Переключение нажатием на точку/заголовок
+const pressOnElement = (index) => {
+  makeInactive(currentIndex);
+  changeTextContent(index);
+  currentIndex = index;
+  setEntity(currentIndex);
+  makeActive(currentIndex);
+}
+
+
+//ОБРАБОТКА НАЖАТИЙ:
+//0.Настройка на нулевой элемент
 let currentIndex = 0;
 
+//1.Левая стрелка
 arrowLeft.addEventListener('click', () => {
-
-
-  switch (currentIndex) {
-    case 0:
-      dotOne.style.opacity = 0.3;
-      dotThree.style.opacity = 1;
-      lineOne.classList.remove('brown-hypertext');
-      lineThree.classList.add('brown-hypertext');
-      break;
-    case 1:
-      dotTwo.style.opacity = 0.3;
-      dotOne.style.opacity = 1;
-      lineTwo.classList.remove('brown-hypertext');
-      lineOne.classList.add('brown-hypertext');
-      break;
-    case 2:
-      dotThree.style.opacity = 0.3;
-      dotTwo.style.opacity = 1;
-      lineThree.classList.remove('brown-hypertext');
-      lineTwo.classList.add('brown-hypertext');
-      break;
-  }
+  makeInactive(currentIndex);
 
   if (currentIndex == 0) {
-    setEntity(entities.length - 1);
     currentIndex = entities.length - 1;
   } else {
-    setEntity(currentIndex - 1);
     currentIndex -= 1;
   }
+
+  changeTextContent(currentIndex);
+  setEntity(currentIndex);
+  makeActive(currentIndex);
 })
+
+//2.Правая стрелка
 arrowRight.addEventListener('click', () => {
-  switch (currentIndex) {
-    case 0:
-      dotOne.style.opacity = 0.3;
-      dotTwo.style.opacity = 1;
-      lineOne.classList.remove('brown-hypertext');
-      lineTwo.classList.add('brown-hypertext');
-      break;
-    case 1:
-      dotTwo.style.opacity = 0.3;
-      dotThree.style.opacity = 1;
-      lineTwo.classList.remove('brown-hypertext');
-      lineThree.classList.add('brown-hypertext');
-      break;
-    case 2:
-      dotThree.style.opacity = 0.3;
-      dotOne.style.opacity = 1;
-      lineThree.classList.remove('brown-hypertext');
-      lineOne.classList.add('brown-hypertext');
-      break;
-  }
+  makeInactive(currentIndex);
 
   if (currentIndex == entities.length - 1) {
-    setEntity(0);
     currentIndex = 0;
   } else {
-    setEntity(currentIndex + 1);
     currentIndex += 1;
   }
+
+  changeTextContent(currentIndex);
+  setEntity(currentIndex);
+  makeActive(currentIndex);
 })
+
+//3.Точка и Заголовок
+for (let i = 0; i <= entities.length - 1; i++) {
+  entities[i].dot.addEventListener('click', () => {
+    pressOnElement(i);
+  });
+  entities[i].line.addEventListener('click', () => {
+    pressOnElement(i);
+  });
+}
